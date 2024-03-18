8695afc5e201d3bf2789109cf812d8828cff3f19
TueVy <155282587+TueVy@users.noreply.github.com>
2024-03-18 23:07:46 +0700

    editInfo.js

--- editInfo.js
+++ editInfo.js
@@ -1,1 +1,12 @@
+function editPersonalInfo() {
+  let info = document.getElementById('personal-info-content');
+  let currentInfo = info.innerHTML;
+  let editableText = <textarea id="info-edit" rows="10" cols="50">${currentInfo}</textarea>;
+  editableText += <button onclick="savePersonalInfo()">Save</button>;
+  info.innerHTML = editableText;
+}
 
+function savePersonalInfo() {
+  let editedInfo = document.getElementById('info-edit').value;
+  document.getElementById('personal-info-content').innerHTML = editedInfo;
+}
